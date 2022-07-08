a0bc9ec10e4a49d231e76cc62480f30509935a59
anderson nascimento dos santos <anderson@nvgo.com.br>
2022-07-08 16:09:32 -0300

    feat: interfces to posts and comments

--- estagio_react/src/interface/index.ts
+++ estagio_react/src/interface/index.ts
@@ -37,3 +37,16 @@ export interface iPhotos {
   url: string;
   thumbnailUrl: string;
 }
+export interface iPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+export interface iComment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
